8c8af069ccad829a8907b040631685d0613c85f6
Matheus Galantine <matgalantine@hotmail.com>
2021-07-21 23:11:55 -0300

    exercicio 1

--- starwars.js
+++ starwars.js
@@ -4,8 +4,15 @@
 //  - Pegar a lista de filmes (AJAX) e preencher no HTML
 //  - Quando um filme for clicado, exibir sua introdução
 
-import friendlyFetch from 'friendly-fetch.js'
-import restartAnimation from 'restart-animation.js'
-import AudioPlayer from 'music-sem-private.js'
+import {friendlyFetch} from '/friendly-fetch.js'
+import {restartAnimation} from '/restart-animation.js'
+import {play} from '/music-sem-private.js'
 
 const API_ENDPOINT = 'https://swapi.dev/api'
+
+const audioUrl = '/audio/tema-sw.mp3'
+const coverImageUrl = '/imgs/logo.svg'
+const title = 'Intro'
+const artist = 'John Williams'
+
+play({audioUrl,coverImageUrl,title,artist}, document.body)
